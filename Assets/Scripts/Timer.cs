9d97eb667b7cc03b8c3bb182803d20a0cf3dc7ac
Sabrina Mackay <mcksab001@myuct.ac.za>
2020-08-30 12:03:55 +0200

    Educational scenes and Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+
+{    
+	private float currentTime;
+	private float startTime =0;
+	private Text theText;
+// Start is called before the first frame update
+    void Start()
+    {
+    	theText = GetComponent<Text>();
+    	currentTime = startTime;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	currentTime += Time.deltaTime;
+        decimal timeDisplay = Decimal.Round(Convert.ToDecimal(currentTime),1);
+        theText.text = "Time: " + timeDisplay + " sec";
+    }
+}
